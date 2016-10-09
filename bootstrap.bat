6061c36000099e5e24d8bdf9b552429209c8bbc4
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2016-10-09 00:29:04 -0500

    Starting bootstrap.bat to automate WSL env setup

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,2 +1,14 @@
 @ECHO off
-bash
+ECHO Bootstrapping WSL environmnet for user:  %USERNAME%
+
+ECHO Allowing Sideloading of Apps...
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowAllTrustedApps" /d "1"
+
+ECHO Putting machine into Developer Mode...
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
+
+ECHO Turning on WSL...
+dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux
+
+ECHO Building WSL environment...
+lxrun /install /y
